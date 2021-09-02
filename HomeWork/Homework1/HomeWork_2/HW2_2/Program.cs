2ec43745e98d3873bb9b9edd2d86646f8c01f3bd
Speak91 <i-valentin@mail.ru>
2021-09-02 21:30:32 +0900

    Выполнено 2 задание 2 урока

---
+++ HomeWork/Homework1/HomeWork_2/HW2_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HW2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            int a = BinarySearch(arr, 2);
+            Console.WriteLine(a);
+        }
+        public static int BinarySearch(int[] inputArray, int searchValue)
+        {
+            int min = 0;
+            int max = inputArray.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (searchValue == inputArray[mid])
+                {
+                    return mid;
+                }
+                else if (searchValue < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+
+}
--- HomeWork/Homework1/HomeWork_2/Hw2_1/Program.cs
+++ HomeWork/Homework1/HomeWork_2/Hw2_1/Program.cs
@@ -7,7 +7,26 @@ namespace Hw2_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LinkedList linkedList = new LinkedList();
+            linkedList.AddNode(1);
+            linkedList.AddNode(2);
+            linkedList.AddNode(3);
+            linkedList.AddNode(4);
+            linkedList.AddNode(5);
+
+            PrintList(linkedList);
+            linkedList.RemoveNode(2);
+            PrintList(linkedList);
+        }
+        static void PrintList(LinkedList list)
+        {
+            var node = list.Head;
+            while (node != null)
+            {
+                Console.WriteLine(node.Value + "");
+                node = node.NextNode;
+            }
+            Console.WriteLine();
         }
     }
 }
