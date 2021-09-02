cc9571366ded1abe1e0d3a509c038d33ad355f1c
Speak91 <i-valentin@mail.ru>
2021-09-02 21:40:07 +0900

    Исправлено 2 задание и добавлены тесты

---
+++ HomeWork/Homework1/HomeWork_2/HW2_2/BinarySearch.cs
@@ -0,0 +1,29 @@
+﻿namespace HW2_2
+{
+    public static class BinarySearch
+    {
+        public static int Search(int[] inputArray, int searchValue)
+        {
+            int min = 0;
+            int max = inputArray.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (searchValue == inputArray[mid])
+                {
+                    return mid;
+                }
+                else if (searchValue < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+
+}
--- HomeWork/Homework1/HomeWork_2/HW2_2/Program.cs
+++ HomeWork/Homework1/HomeWork_2/HW2_2/Program.cs
@@ -2,6 +2,7 @@
 
 namespace HW2_2
 {
+
     class Program
     {
         static void Main(string[] args)
@@ -10,28 +11,7 @@ namespace HW2_2
             int a = BinarySearch(arr, 2);
             Console.WriteLine(a);
         }
-        public static int BinarySearch(int[] inputArray, int searchValue)
-        {
-            int min = 0;
-            int max = inputArray.Length - 1;
-            while (min <= max)
-            {
-                int mid = (min + max) / 2;
-                if (searchValue == inputArray[mid])
-                {
-                    return mid;
-                }
-                else if (searchValue < inputArray[mid])
-                {
-                    max = mid - 1;
-                }
-                else
-                {
-                    min = mid + 1;
-                }
-            }
-            return -1;
-        }
+        
     }
 
 }
