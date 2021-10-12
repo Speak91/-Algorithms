49df61e740924a233302cc3ecf6a8d094e284b92
Speak91 <i-valentin@mail.ru>
2021-10-12 11:06:14 +0900

    Выполнено дз 8 урока

---
+++ HomeWork/Homework1/HW8_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HW8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BucketSortClass bucketSortClass = new BucketSortClass();
+            var arrayToSort = GetRandomArray(50, 1, 100);
+            Console.WriteLine("Начальный массив: " + string.Join(", ", arrayToSort) + "\n");
+            Console.WriteLine("Отсортированный массив: " + string.Join(", ",bucketSortClass.BucketSortMethod(arrayToSort)));
+            Console.ReadLine();
+        }
+        static int[] GetRandomArray(int arraySize, int minValue, int maxValue)
+        {
+            var random = new Random();
+            var randomArray = new int[arraySize];
+            for (var i = 0; i < randomArray.Length; i++)
+            {
+                randomArray[i] = random.Next(minValue, maxValue);
+            }
+
+            return randomArray;
+        }
+    }
+}
